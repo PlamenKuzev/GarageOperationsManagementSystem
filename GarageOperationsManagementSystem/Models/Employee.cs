2722674dcd253af1ad92abd1235e967ef81f0606
Plamen_K <plamenkuzev11@gmail.com>
2026-03-06 07:21:10 +0200

    Add Employee entity model

---
+++ GarageOperationsManagementSystem/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿namespace GarageOperationsManagementSystem.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Position { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public DateTime WorkingSince { get; set; }
+
+        public int GarageId { get; set; }
+
+        public Garage Garage { get; set; }
+    }
+}
