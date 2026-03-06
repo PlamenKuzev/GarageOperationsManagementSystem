e71ff571b15178bedf9aeaafec0a0fae401ded9d
Plamen_K <plamenkuzev11@gmail.com>
2026-03-06 07:24:27 +0200

    Add RepairOrder entity model

---
+++ GarageOperationsManagementSystem/Models/RepairOrder.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GarageOperationsManagementSystem.Models
+{
+    public class RepairOrder
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string IssueCode { get; set; } = null!;
+
+        public string IssueDescription { get; set; }
+
+        public DateTime ArrivalDate { get; set; }
+
+        public DateTime? CompletionDate { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        public decimal? ReapairPrice { get; set; }
+
+        public int CarId { get; set; }
+
+        public Car Car { get; set; }
+
+        public int GarageId { get; set; }
+
+        public Garage Garage { get; set; }
+
+    }
+}
