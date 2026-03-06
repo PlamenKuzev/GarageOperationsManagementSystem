59733e7e02c596600d05215c6022d4d44620cf98
Plamen_K <plamenkuzev11@gmail.com>
2026-03-06 07:30:57 +0200

    Register all entity models in ApplicationDbContext

--- GarageOperationsManagementSystem/Data/ApplicationDbContext.cs
+++ GarageOperationsManagementSystem/Data/ApplicationDbContext.cs
@@ -1,13 +1,21 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using GarageOperationsManagementSystem.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace GarageOperationsManagementSystem.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+
+        public DbSet<Garage> Garages { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<RepairOrder> RepairOrders { get; set; }
+
+
+
     }
 }
