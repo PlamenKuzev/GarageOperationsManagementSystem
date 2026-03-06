c3d81af8bc52cd5d3ecf37d7e7a1924e5059ebdc
Plamen_K <plamenkuzev11@gmail.com>
2026-03-06 07:09:07 +0200

    Add Garage entity model

---
+++ GarageOperationsManagementSystem/Models/Garage.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GarageOperationsManagementSystem.Models
+{
+    public class Garage
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        public int Capacity { get; set; }
+
+        public string WorkSchedule { get; set; }
+
+        //public ICollection<Employee> Employees { get; set; } = new List<Employee>();
+
+        //public ICollection<RepairOrder> RepairOrders { get; set; } = new List<RepairOrder>();
+
+
+    }
+}
