a9830fa8b209a2d6549e39e54e425bee263baaa1
Plamen_K <plamenkuzev11@gmail.com>
2026-03-06 07:18:10 +0200

    Add Car entity model

---
+++ GarageOperationsManagementSystem/Models/Car.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GarageOperationsManagementSystem.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+
+        public string Brand { get; set; }
+
+        [Required]
+        public string Model { get; set; } = null!;
+
+        public int? Year { get; set; }
+
+        public int? Mileage { get; set; }
+
+        public int OwnerId { get; set; }
+
+        public Owner Owner { get; set; }
+
+        //public ICollection<RepairOrder> RepairOrders { get; set; } = new List<RepairOrder>();
+    }
+}
