3c94e9b094ae3c03d12a24ab8f283516e40a3dc8
Plamen_K <plamenkuzev11@gmail.com>
2026-03-06 07:14:03 +0200

    Add Owner entity model

---
+++ GarageOperationsManagementSystem/Models/Owner.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GarageOperationsManagementSystem.Models
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; } = null!;
+
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        //public ICollection<Car> Cars { get; set; } = new List<Car>();
+    }
+}
