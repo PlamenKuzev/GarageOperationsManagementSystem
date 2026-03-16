8cd557a79f26da818d0c57ea554bf395e8b35b6b
Plamen_K <plamenkuzev11@gmail.com>
2026-03-17 00:50:51 +0200

    Add IRepairOrderService interface

---
+++ GarageOperationsManagementSystem/Interfaces/IRepairOrderService.cs
@@ -0,0 +1,16 @@
+﻿using GarageOperationsManagementSystem.Models;
+
+namespace GarageOperationsManagementSystem.Interfaces
+{
+    public interface IRepairOrderService
+    {
+        Task<IEnumerable<RepairOrder>> GetAllOrdersAsync();
+
+        Task<RepairOrder> GetOrderByIdAsync(int Id);
+
+        Task CreateOrderAsync(RepairOrder order);
+
+        Task CompleteOrderAsync(int Id, decimal price);
+
+    }
+}
