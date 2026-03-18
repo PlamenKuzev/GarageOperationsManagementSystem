056d0c7348eb03e0e0c3792f05ca470211c732d8
Plamen_K <plamenkuzev11@gmail.com>
2026-03-18 05:33:13 +0200

    Update database after introducing ApplicationUser

---
+++ GarageOperationsManagementSystem/Migrations/20260318032832_AddApplicationUser.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GarageOperationsManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddApplicationUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ReapairPrice",
+                table: "RepairOrders",
+                newName: "RepairPrice");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "RepairPrice",
+                table: "RepairOrders",
+                newName: "ReapairPrice");
+        }
+    }
+}
