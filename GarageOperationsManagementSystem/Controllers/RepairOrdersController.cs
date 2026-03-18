880d7e2606ca275c315d3ae9f77f524ea08f7ee2
Plamen_K <plamenkuzev11@gmail.com>
2026-03-18 04:24:34 +0200

    Add RepairOrdersController with repair lifecycle actions

---
+++ GarageOperationsManagementSystem/Controllers/RepairOrdersController.cs
@@ -0,0 +1,49 @@
+﻿using GarageOperationsManagementSystem.Interfaces;
+using GarageOperationsManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GarageOperationsManagementSystem.Controllers
+{
+    public class RepairOrdersController : Controller
+    {
+        private readonly IRepairOrderService _repairService;
+
+        public RepairOrdersController(IRepairOrderService repairService)
+        {
+            _repairService = repairService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var orders = await _repairService.GetAllOrdersAsync();
+            return View(orders);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(RepairOrder order)
+        {
+            if (ModelState.IsValid)
+            {
+                order.ArrivalDate = DateTime.Now;
+                order.IssueCode = Guid.NewGuid().ToString().Substring(0, 8);
+
+                await _repairService.CreateOrderAsync(order);
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(order);
+        }
+
+        public async Task<IActionResult> Complete(int id)
+        {
+            await _repairService.CompleteOrderAsync(id, 100);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
