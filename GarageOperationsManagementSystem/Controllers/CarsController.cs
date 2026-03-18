e50f4f0d8fcfa55c25d9f065aa554892ea07b6af
Plamen_K <plamenkuzev11@gmail.com>
2026-03-18 03:53:31 +0200

    Add CarsController using service layer

---
+++ GarageOperationsManagementSystem/Controllers/CarsController.cs
@@ -0,0 +1,61 @@
+﻿using GarageOperationsManagementSystem.Interfaces;
+using GarageOperationsManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace GarageOperationsManagementSystem.Controllers
+{
+    public class CarsController : Controller
+    {
+        private readonly ICarService _carService;
+
+        public CarsController(ICarService carService)
+        {
+            _carService = carService;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            var cars = await _carService.GetAllCarsAsync();
+            return View(cars);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Car car)
+        {
+            if (ModelState.IsValid)
+            {
+                await _carService.CreateCarAsync(car);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(car);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var car = await _carService.GetCarByIdAsync(id);
+
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            return View(car);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _carService.DeleteCarAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+ 
+    }
+}
