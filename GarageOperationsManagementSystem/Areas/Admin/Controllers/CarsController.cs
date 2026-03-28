fb2d8760971ffd8273853908f93cdca728856ebd
Plamen_K <plamenkuzev11@gmail.com>
2026-03-28 03:24:20 +0200

    Update CarsController for Admin area

--- GarageOperationsManagementSystem/Areas/Admin/Controllers/CarsController.cs
+++ GarageOperationsManagementSystem/Areas/Admin/Controllers/CarsController.cs
@@ -1,8 +1,10 @@
-﻿using GarageOperationsManagementSystem.Interfaces;
+﻿using GarageOperationsManagementSystem.Data;
+using GarageOperationsManagementSystem.Interfaces;
 using GarageOperationsManagementSystem.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace GarageOperationsManagementSystem.Areas.Admin.Controllers
 {
@@ -11,40 +13,85 @@ namespace GarageOperationsManagementSystem.Areas.Admin.Controllers
     public class CarsController : Controller
     {
         private readonly ICarService _carService;
+        private readonly ApplicationDbContext _context;
 
-        public CarsController(ICarService carService)
+        public CarsController(ICarService carService, ApplicationDbContext context)
         {
             _carService = carService;
+            _context = context;
         }
-        
+
         public async Task<IActionResult> Index()
         {
             var cars = await _carService.GetAllCarsAsync();
             return View(cars);
         }
 
-        public IActionResult Create()
+        public async Task<IActionResult> Details(int id)
         {
+            var car = await _carService.GetCarByIdAsync(id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            return View(car);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            await PopulateOwnerSelectAsync();
             return View();
         }
 
-
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Car car)
+        public async Task<IActionResult> Create([Bind("Brand", "Model", "Year", "Mileage", "OwnerId")] Car car)
         {
             if (ModelState.IsValid)
             {
                 await _carService.CreateCarAsync(car);
                 return RedirectToAction(nameof(Index));
             }
+
+            await PopulateOwnerSelectAsync(car.OwnerId);
+            return View(car);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var car = await _carService.GetCarByIdAsync(id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            await PopulateOwnerSelectAsync(car.OwnerId);
+            return View(car);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id", "Brand", "Model", "Year", "Mileage", "OwnerId")] Car car)
+        {
+            if (id != car.Id)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _carService.UpdateCarAsync(car);
+                return RedirectToAction(nameof(Index));
+            }
+
+            await PopulateOwnerSelectAsync(car.OwnerId);
             return View(car);
         }
 
         public async Task<IActionResult> Delete(int id)
         {
             var car = await _carService.GetCarByIdAsync(id);
-
             if (car == null)
             {
                 return NotFound();
@@ -54,11 +101,20 @@ namespace GarageOperationsManagementSystem.Areas.Admin.Controllers
         }
 
         [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             await _carService.DeleteCarAsync(id);
             return RedirectToAction(nameof(Index));
         }
- 
+
+        private async Task PopulateOwnerSelectAsync(int? selectedOwnerId = null)
+        {
+            var owners = await _context.Owners
+                .OrderBy(o => o.FullName)
+                .ToListAsync();
+
+            ViewData["OwnerId"] = new SelectList(owners, "Id", "FullName", selectedOwnerId);
+        }
     }
 }
